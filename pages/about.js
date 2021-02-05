23668fc26c340006835e286b6590da969ac2fe29
unknown <you@example.com>
2021-02-05 17:42:53 +0200

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>Daniel Shokri - About me</title>
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
